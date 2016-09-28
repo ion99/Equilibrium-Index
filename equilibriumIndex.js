c419250d178497ea9d55748d4c85134461b86c89
Ion Gorincioi <gorincioii@yahoo.com>
2016-09-27 23:28:33 -0700

    Update the solution-use reduce

--- equilibriumIndex.js
+++ equilibriumIndex.js
@@ -1,17 +1,21 @@
 function solution(array) {
-  function sum(arr){
-    let res = 0;
-    arr.forEach(i => res+=i);
-    return res;
-  }
+  let result = [];
+  // function sum(arr){
+  //   let res = 0;
+  //   arr.forEach(i => res+=i);
+  //   return res;
+  // }
   for (let i = 0; i<array.length; i++){
-    if(sum(array.slice(0,i)) === sum(array.slice(i+1,array.length))){
-      return 1;
-    } else {
-      continue;
-    }
+    // if(sum(array.slice(0,i)) === sum(array.slice(i+1,array.length))){
+    if(array.slice(0,i).reduce(function(a,b){return a+b},0) === array.slice(i+1,array.length).reduce(function(a,b){return a+b},0)) {
+      result.push(i);
+    } 
   }
-  return -1;
+  if (result.length){
+    return result;
+  } else {
+    return -1;
+  }
 }
 
-//solution([-1, 3, -4, 5, 1, -6, 3, 1]);
+//solution([-1, 3, -4, 5, 1, -6, 2, 1]);
